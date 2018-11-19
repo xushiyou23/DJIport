a1ecc955f541360e8f05c68ac57d1219ca688935
1175337619 <1175337619@qq.com>
2018-11-19 09:44:50 +0800

    1
    DjiApi Create

---
+++ DjiAPI_port/DjiAPI_port/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DjiAPI_port
+//
+//  Created by xujiahui on 2018/11/19.
+//  Copyright © 2018年 xujiahui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DjiAPI_port/DjiAPI_port/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DjiAPI_port
+//
+//  Created by xujiahui on 2018/11/19.
+//  Copyright © 2018年 xujiahui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
